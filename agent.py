8426343fd0f4c179c46fc92d20b35326c66009ed
Jonas Neumann <jonas.a.neumann@accenture.com>
2025-05-21 12:58:44 +0800

    changed model to gemma3-12b-it-4bit-DWQ

--- agent.py
+++ agent.py
@@ -5,8 +5,12 @@ from smolagents import (
 )
 
 # model = MLXModel(model_id="mlx-community/SmolLM2-1.7B-Instruct")
-model = MLXModel(model_id="mlx-community/Mamba-Codestral-7B-v0.1")
 # model = MLXModel(model_id="mlx-community/Mamba-Codestral-7B-v0.1")
+# model = MLXModel(model_id="mlx-community/Josiefied-Qwen2.5-Coder-7B-Instruct-abliterated-v1")
+# model = MLXModel(model_id="mlx-community/Josiefied-Qwen2.5-3B-Instruct-abliterated-v1-8-bit")
+# model = MLXModel(model_id="mlx-community/Josiefied-Qwen3-4B-abliterated-v1-8bit")
+# model = MLXModel(model_id="mlx-community/gemma-3-4b-it-4bit-DWQ")
+model = MLXModel(model_id="mlx-community/gemma-3-12b-it-4bit-DWQ")
 prompt = "How many seconds would it take for a leopard at full speed to run through Pont des Arts?"
 
 agent = CodeAgent(
